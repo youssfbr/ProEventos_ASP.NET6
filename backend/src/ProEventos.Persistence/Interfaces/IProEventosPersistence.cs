ec690e1b652d09bc76ebc855d376cb7cbafd4b47
Alisson Youssf <youssfbr@gmail.com>
2021-10-15 20:15:50 -0300

    Interface

---
+++ backend/src/ProEventos.Persistence/Interfaces/IProEventosPersistence.cs
@@ -0,0 +1,21 @@
+using ProEventos.Domain.Models;
+
+namespace ProEventos.Persistence.Interfaces
+{
+    public interface IProEventosPersistence
+    {
+        void Add<T>(T entity) where T: class;
+        void Update<T>(T entity) where T: class;
+        void Delete<T>(T entity) where T: class;
+        void DeleteRange<T>(T[] entity) where T: class;
+        Task<bool> SaveChangesAsync();
+
+        Task<Evento[]> GetAllEventosAsync(bool includePalestrantes);
+        Task<Evento[]> GetAllEventosByTemaAsync(string tema, bool includePalestrantes);
+        Task<Evento> GetEventoByIdAsync(int id, bool includePalestrantes);
+
+        Task<Palestrante[]> GetAllPalestrantesAsync(bool includeEventos);
+        Task<Palestrante[]> GetAllPalestrantesByNomeAsync(string nome, bool includeEventos);
+        Task<Palestrante> GetPalestranteByIdAsync(int id, bool includeEventos);
+    }
+}
