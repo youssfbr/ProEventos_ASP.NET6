5dcd2307ca629e404be9aa0e12602c4596b626c1
Alisson Youssf <youssfbr@gmail.com>
2021-10-15 08:50:33 -0300

    Get mock test

--- backend/src/ProEventos.API/Controllers/EventoController.cs
+++ backend/src/ProEventos.API/Controllers/EventoController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers;
 
@@ -11,8 +12,15 @@ public class EventoController : ControllerBase
     }
 
     [HttpGet]
-    public string Get()
+    public Evento Get()
     {
-        return "value";
+        return new Evento() {
+            Id = 1,
+            Tema = "Angular e .NET 6",
+            Local = "Fortaleza",
+            Lote = "1o lote",
+            QtsPessoas = 250,
+            DataEvento = DateTime.Now.AddDays(2).ToString()  
+        };
     }
 }
