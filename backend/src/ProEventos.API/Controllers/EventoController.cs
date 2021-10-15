33ce7ab3ce97fc38071f7d2827ad9c1a34002f82
Alisson Youssf <youssfbr@gmail.com>
2021-10-15 09:07:52 -0300

    Get[] mock test

--- backend/src/ProEventos.API/Controllers/EventoController.cs
+++ backend/src/ProEventos.API/Controllers/EventoController.cs
@@ -7,20 +7,38 @@ namespace ProEventos.API.Controllers;
 [Route("api/[controller]")]
 public class EventoController : ControllerBase
 { 
-    public EventoController()
-    {        
-    }
-
-    [HttpGet]
-    public Evento Get()
+    private IEnumerable<Evento> _eventos = new Evento[] 
     {
-        return new Evento() {
+        new Evento() 
+        {
             Id = 1,
             Tema = "Angular e .NET 6",
             Local = "Fortaleza",
             Lote = "1o lote",
             QtsPessoas = 250,
-            DataEvento = DateTime.Now.AddDays(2).ToString()  
-        };
+            DataEvento = DateTime.Now.AddDays(2).ToString(),
+            ImagemURL = "foto1.png"  
+        },
+        new Evento() 
+        {
+            Id = 2,
+            Tema = ".NET 6 e suas novidades",
+            Local = "Caucaia",
+            Lote = "2o lote",
+            QtsPessoas = 350,
+            DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy"),
+            ImagemURL = "foto2.png"  
+        }
+    };
+
+    public EventoController()
+    {        
     }
+
+    [HttpGet]
+    public IEnumerable<Evento> Get()
+    {
+        return _eventos;
+    }
+
 }
