49fcc58a1b614bc42e7e1ef1dc1577cb680a22b9
Alisson Youssf <youssfbr@gmail.com>
2021-10-19 16:51:20 -0300

    Angular: static

--- frontend/ProEventos-App/src/app/eventos/eventos.component.ts
+++ frontend/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -7,30 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public evento: any = {
-    Tema: 'Angular',
-    Local: 'Fortaleza'
-  }
-
-  public eventos: any = [
-    {
-      Tema: 'Angular 12',
-      Local: 'Fortaleza'
-    },
-    {
-      Tema: '.NET 6',
-      Local: 'Caucaia'
-    },
-    {
-      Tema: 'Angular e suas novidades',
-      Local: 'Iguape'
-    }
-  ]
-
+  public eventos: any;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.getEventos();
+  }
+
+  public getEventos(): void {
+
+    this.eventos  = [
+      {
+        Tema: 'Angular 12',
+        Local: 'Fortaleza'
+      },
+      {
+        Tema: '.NET 6',
+        Local: 'Caucaia'
+      },
+      {
+        Tema: 'Angular e suas novidades',
+        Local: 'Iguape'
+      }
+    ]
+
   }
 
 }
