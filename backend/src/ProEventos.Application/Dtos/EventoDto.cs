b47451658dc2ef53e42a473593397c6c0dc778bd
Alisson Youssf <youssfbr@gmail.com>
2021-10-19 13:15:59 -0300

    DataAnnotations

--- backend/src/ProEventos.Application/Dtos/EventoDto.cs
+++ backend/src/ProEventos.Application/Dtos/EventoDto.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ProEventos.Application.Dtos
 {
     public class EventoDto
@@ -5,10 +7,28 @@ namespace ProEventos.Application.Dtos
         public int Id { get; set; }
         public string Local { get; set; }
         public string DataEvento { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório."),
+         //StringLength(50,   MinimumLength = 3, 
+         //                   ErrorMessage = "Intervalo permitido de 3 a 50 caracteres")]
+         MinLength(3, ErrorMessage = "{0} deve ter no mínimo 4 caracteres."),
+         MaxLength(50, ErrorMessage = "{0} deve ter no máximo 50 caracteres.")]
         public string Tema { get; set; }
-        public int QtsPessoas { get; set; }         
+
+        [Display(Name = "Qtd Pessoas")]
+        [Range(1, 120000, ErrorMessage = "{0} não pode ser menor que 1 e maior que 120.000")]
+        public int QtdPessoas { get; set; }   
+
+        [RegularExpression(@".*\.(gif|jpe?g|bmp|png)$",
+                            ErrorMessage = "Não é uma imagem válida. (gif, jpg, jpeg, bmp ou png)")]      
         public string ImagemURL { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+        [Phone(ErrorMessage = "O campo {0} está com número inválido.")]
         public string Telefone { get; set; }
+
+        [Display(Name = "e-mail")]
+        [EmailAddress(ErrorMessage = "O campo {0} deve ser um e-mail válido.")]
         public string Email { get; set; }
         
         public IEnumerable<LoteDto> Lotes { get; set; }
--- backend/src/ProEventos.Domain/Models/Evento.cs
+++ backend/src/ProEventos.Domain/Models/Evento.cs
@@ -6,7 +6,7 @@ namespace ProEventos.Domain.Models
         public string Local { get; set; }
         public DateTime? DataEvento { get; set; }
         public string Tema { get; set; }
-        public int QtsPessoas { get; set; }         
+        public int QtdPessoas { get; set; }         
         public string ImagemURL { get; set; }
         public string Telefone { get; set; }
         public string Email { get; set; }
