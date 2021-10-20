13c5be06bd5d9a66a3938b0bb45523ad5072b0b6
Alisson Youssf <youssfbr@gmail.com>
2021-10-20 09:10:59 -0300

    *ngIf

--- frontend/ProEventos-App/src/app/eventos/eventos.component.ts
+++ frontend/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any = [];
 
   constructor(private http: HttpClient) { }
 
@@ -17,7 +17,7 @@ export class EventosComponent implements OnInit {
   }
 
   public getEventos(): void {
-    this.http.get('https://localhost:7234/api/eventos').subscribe(
+   this.http.get('https://localhost:7234/api/eventos').subscribe(
       response => this.eventos = response,
       error => console.log(error)
     );
