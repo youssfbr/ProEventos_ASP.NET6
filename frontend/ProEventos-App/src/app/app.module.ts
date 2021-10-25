c59e03aa1a9ffc2cc70ffe1c36d76222284090c4
Alisson Youssf <youssfbr@gmail.com>
2021-10-25 11:05:30 -0300

    Angular: Modal

--- frontend/ProEventos-App/src/app/app.module.ts
+++ frontend/ProEventos-App/src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { EventoService } from './services/evento.service';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
 import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { ModalModule } from 'ngx-bootstrap/modal';
 
+import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { PalestrantesComponent } from './palestrantes/palestrantes.component';
+
+import { EventoService } from './services/evento.service';
 
 @NgModule({
   declarations: [
@@ -28,7 +31,9 @@ import { PalestrantesComponent } from './palestrantes/palestrantes.component';
     FormsModule,
     BrowserAnimationsModule,
     TooltipModule.forRoot(),
-    CollapseModule.forRoot()
+    CollapseModule.forRoot(),
+    BsDropdownModule.forRoot(),
+    ModalModule.forRoot(),
   ],
   providers: [EventoService],
   bootstrap: [AppComponent]
--- frontend/ProEventos-App/src/app/eventos/eventos.component.ts
+++ frontend/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
 import { EventoService } from './../services/evento.service';
+
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 
 import { Evento } from './../models/Evento';
 
@@ -19,6 +21,17 @@ export class EventosComponent implements OnInit {
   public exibirImagem = true;
   private _filtroListado = '';
 
+  modalRef?: BsModalRef;
+
+  constructor(
+    private eventoService: EventoService,
+    private modalService: BsModalService
+  ) { }
+
+  public ngOnInit(): void {
+    this.getEventos();
+  }
+
   public get filtroLista(): string {
     return this._filtroListado;
   }
@@ -36,24 +49,32 @@ export class EventosComponent implements OnInit {
     );
   }
 
-  constructor(private eventoService: EventoService) { }
-
-  public ngOnInit(): void {
-    this.getEventos();
-  }
-
   public alterarImagem(): void {
     this.exibirImagem = !this.exibirImagem;
   }
 
   public getEventos(): void {
-   this.eventoService.getEventos().subscribe({
-     next: (eventos: Evento[]) => {
-      this.eventos = eventos;
-      this.eventosFiltrados = this.eventos;
-    },
-    error: (error: any) => console.log(error)
-   });
+    this.eventoService.getEventos().subscribe({
+      next: (eventos: Evento[]) => {
+        this.eventos = eventos;
+        this.eventosFiltrados = this.eventos;
+      },
+      error: (error: any) => console.log(error)
+    });
   }
+
+  /* Modal */ ////////////////////////////////////////////////////////////
+  openModal(template: TemplateRef<any>): void {
+    this.modalRef = this.modalService.show(template, {class: 'modal-sm'});
+  }
+
+  confirm(): void {
+    this.modalRef?.hide();
+  }
+
+  decline(): void {
+    this.modalRef?.hide();
+  }
+  /* Modal Fim */ /////////////////////////////////////////////////////////////
 
 }
