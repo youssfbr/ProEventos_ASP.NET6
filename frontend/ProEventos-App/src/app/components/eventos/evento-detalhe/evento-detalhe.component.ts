51152aba67f86c9d0661590deb9c3d17ef784cb6
Alisson Youssf <youssfbr@gmail.com>
2021-10-28 13:03:52 -0300

    Angular: tsconfig setup, ReactiveFormsModule

--- frontend/ProEventos-App/src/app/components/eventos/evento-detalhe/evento-detalhe.component.ts
+++ frontend/ProEventos-App/src/app/components/eventos/evento-detalhe/evento-detalhe.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-evento-detalhe',
@@ -7,9 +8,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventoDetalheComponent implements OnInit {
 
+  form: FormGroup;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  validation(): void {
+    this.form = new FormGroup({
+      local: new FormControl(),
+      dataEvento: new FormControl(),
+      tema: new FormControl(),
+      qtdPessoas: new FormControl(),
+      imagemURL: new FormControl(),
+      telefone: new FormControl(),
+      email: new FormControl(),
+    });
   }
 
 }
