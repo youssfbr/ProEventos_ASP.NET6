420bb798bd1ce9baf9307ccd7a48e3d5f4c847a9
Alisson Youssf <youssfbr@gmail.com>
2021-10-29 13:24:40 -0300

    Angular: User/registration Validation FormBuilder

--- frontend/ProEventos-App/src/app/components/eventos/evento-detalhe/evento-detalhe.component.ts
+++ frontend/ProEventos-App/src/app/components/eventos/evento-detalhe/evento-detalhe.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-evento-detalhe',
@@ -32,7 +32,7 @@ export class EventoDetalheComponent implements OnInit {
     });
   }
 
-  resetForm(): void { 
+  resetForm(): void {
     this.form.reset();
   }
 
--- frontend/ProEventos-App/src/app/components/user/registration/registration.component.ts
+++ frontend/ProEventos-App/src/app/components/user/registration/registration.component.ts
@@ -1,3 +1,4 @@
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +8,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup;
+
+  get f(): any {
+    return this.form.controls;
+  }
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.validation();
+  }
+
+  validation(): void {
+    this.form = this.fb.group({
+      primeiroNome: ['', [Validators.required]],
+      ultimoNome: ['', [Validators.required]],
+      nomeCompleto: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      usuario: ['', [Validators.required]],
+      senha: ['', [Validators.required]],
+      confirmarSenha: ['', [Validators.required]],
+    });
+  }
+
+  resetForm(): void {
+    this.form.reset();
   }
 
 }
